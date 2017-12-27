cf0fb11b033cbb13caf8aab1bb9cde498036734e
lunax <albertofly10@gmail.com>
2017-12-27 11:47:48 +0100

    Merge master into JobService

--- app/src/main/java/com/bignerdranch/android/photogallery/GalleryItem.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/GalleryItem.java
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.photogallery;
 
+import android.net.Uri;
+
 /**
  * Created by albus on 10-Dec-17.
  */
@@ -8,6 +10,24 @@ public class GalleryItem {
     private String mCaption;
     private String mId;
     private String mUrl;
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+
+    public Uri getPhotoPageUri(){
+        return Uri.parse("https://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
+    }
+
+    private String mOwner;
 
     public String getCaption() {
         return mCaption;
